3f3ef4e2fda220d481a132d7231ca93306b23b57
KariTrace <kari.eve.trace@gmail.com>
2013-06-15 16:44:41 -0400

    index.php / includes/header.php = Abstract HTML head elements to a PHP include. DRY concept.

--- index.php
+++ index.php
@@ -1,31 +1,12 @@
 <?php
+    //auto load here, need to get use to autoloading and using libs
     $vendor     = "vendor/";
     $components = "components/";
 ?>
 <!DOCTYPE html>
-<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
-<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
-<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
-<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-        <title>EVE Gate to Google Calendar</title>
-        <meta name="description" content="">
-        <meta name="viewport" content="width=device-width">
 
-        <!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
+    <?php include_once("includes/header.php"); ?>
 
-        <!-- CSS includes -->
-        <link rel="stylesheet" href="<?= $components; ?>require.css">
-        <link rel="stylesheet" href="<?= $components; ?>html5-boilerplate/css/main.css">
-        <link rel="stylesheet" href="<?= $components; ?>normalize.css/normalize.css">        
-
-        <!-- JS includes -->
-        <script src="<?= $components; ?>require.js"></script>
-        <script src="<?= $components; ?>jquery/jquery.js"></script>
-        <script src="<?= $components; ?>modernizr/modernizr.js"></script>
-    </head>
     <body>
 		<?php include_once("includes/googleAnalytics.php") ?>
         <!--[if lt IE 7]>
@@ -55,6 +36,7 @@
 
             <input type="button" value="Proceed to Step Two &#61&#62" name="processStepOne" />
         </form>
+
 
 
 
