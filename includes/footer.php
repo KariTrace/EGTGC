7c94348fc3844867a30f5fbf3a6d2c56c950846f
KariTrace <kari.eve.trace@gmail.com>
2013-06-19 18:58:59 -0400

    footer.php = Populated includes/footer.php

---
+++ includes/footer.php
@@ -0,0 +1,22 @@
+
+    <!-- jQ and other end page scripting-->
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+    <script>window.jQuery || document.write('<script src=",.js/vendor/jquery-1.9.1.min.js"><\/script>')</script>
+
+    <script src="//ajax.aspnetcdn.com/ajax/jquery.validate/1.11.1/jquery.validate.min.js"></script>
+    <script>window.jQuery || document.write('<script src="./js/jquery.validate.js"><\/script>')</script>
+    
+    
+    <script src="./js/plugins.js"></script>
+    <script src="./js/main.js"></script>
+    <script><!--
+        $("#processStepOne").on("click", function (event){
+            console.log('here');
+
+            //validate API data:
+            //KeyID: numeric / inf. length: 2270116
+            //vCode: alpha-numeric / 64 length: YNQfkcSflDkeZAPqWWeIxL8XyyZszcmBg1S7j8vZKLoQi4ajLxIbKM2KwJ7sf0k8
+
+        });
+    --></script>
+</html>
