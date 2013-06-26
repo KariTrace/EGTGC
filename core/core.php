45d7852a3232bf20dd44bd83feb2503b3a0fbf37
KariTrace <kari.eve.trace@gmail.com>
2013-06-26 13:43:43 -0400

    ./core/core.php = Removed output()  methods (moved to output class).

--- core/core.php
+++ core/core.php
@@ -43,38 +43,7 @@ class core {
 	}
 
 	/**
-	 * Input processed data, output requested type
-	 * @todo Integrat Monolog Library
-	 * @param void $paramater Data to be returned to the client
-	 * @return  null || void Method response when returning data
 	 */
-	public function output($paramater = null, $is_valid = true) {
 
-		// Just a dump check for now
-		// TODO Add in monolog processing for all output actions
-		if ($is_valid) {
-			echo "$is_valid is false";
-			exit;
-		}
-
-		//return PHP data
-		if (!$paramater->return_type || !$paramater->return_type == null) {
-			return $paramater;
-
-		// Return JSON object
-		} elseif (strtolower($paramater->return_type) == "json") {
-			return json_encode($paramater);
-
-		// Return data dump
-		} elseif (strtolower($paramater->return_type) == "dump") {
-			echo '<PRE>';
-			print_r($parmater);
-			echo '</PRE>';
-			exit;
-		}
-
-		// If all fails, call itself and dump data.
-		$paramater->return_type = "dump";
-		return $this->ouput($paramater);
 	}
 }
