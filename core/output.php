020fb4e3d8cbf5a8a9ec301b21d53ff4cca0d103
KariTrace <kari.eve.trace@gmail.com>
2013-06-26 14:06:41 -0400

    .,/core/output.php = Method parameter changes.

--- core/output.php
+++ core/output.php
@@ -2,22 +2,17 @@
 /**
  * Output Class; any output goes via this class one way or another
  */
+
 class output {
 	/**
 	 * Input processed data, output requested type
 	 * @todo Integrat Monolog Library
-	 * @param void $paramater Data to be returned to the client
-	 * @return  null || void Method response when returning data
+	 * @param stdClass $paramater [required] Data to be returned to the client
+	 * @return null || void Method response when returning data
 	 */
-	public final function main($paramater = null, $is_valid = true)
+	public final function main(stdClass $paramater, $is_valid = true)
 	{
-
-		// Just a dump check for now
-		// TODO Add in monolog processing for all output actions
-		if ($is_valid) {
-			echo "$is_valid is false";
-			exit;
-		}
+		$paramater->return_type = (!isset($paramater->return_type) ? "json" : null);
 
 		//return PHP data
 		if (!$paramater->return_type || !$paramater->return_type == null) {
@@ -44,8 +39,10 @@ class output {
 	 * Debug method, interface for monolog
 	 * @author kari.eve.trace@gmail.com
 	 * @since 2013-06-26
+	 * @param stdClass $paramater [required] Object to be dump printed.
+	 * @return boolean
 	 */
-	public final function debugger($paramater)
+	public final function debugger(stdClass $paramater)
 	{
 		echo'<PRE>';
 		print_r($paramater);
