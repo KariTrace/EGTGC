0b34e247fe88e71dec52c1b406b6561699a48df3
KariTrace <kari.eve.trace@gmail.com>
2013-06-27 09:27:40 -0400

    ./step2.php = Started page 2 (Google API).

---
+++ step2.php
@@ -0,0 +1,51 @@
+<?php
+    include_once('/vendor/autoload.php');
+
+    //auto load here, need to get use to autoloading and using libs
+    $vendor     = "vendor/";
+    $components = "components/";
+?>
+<!DOCTYPE html>
+
+    <?php include_once("includes/header.php"); ?>
+
+    <body>
+		<?php include_once("includes/googleAnalytics.php") ?>
+        <!--[if lt IE 7]>
+            <p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> to improve your experience.</p>
+        <![endif]-->
+
+        <!-- Add your site or application content here -->
+        <h1>Welcome to EGTGC</h1>
+        <h2>4 easy steps and your EVE calendar will be re-produced onto your Google calendar.</h2>
+        <h3>Step 2 of 4: Google API Information</h3>
+
+        <h4><a href="" target="_new">Click here</a> to get a new Google API key pair specific to EGTGC.</h4>
+        <ul>
+            <li class="alert">CHECK THE 'NO EXPIRE' BOX!.</li>
+            <li>Name it EGTGC for easy identification.</li>
+        </ul>
+
+        <h4>Paste the related information into the boxes below.</h4>
+
+        <form id="googleAPIForm" method="post">
+            <lable>Google API Public Key</lable>
+            <input type="text" name="pubKey" value="">
+
+            <lable>EVE API Verification Code</lable>
+            <input type="text" name="privKey" value="">
+
+            <!--// Indicate to the core what form we are-->
+            <input type="hidden" name="form_name" value="googleAPI">
+
+            <input type="button" value="Clear" id="clearStepOne" class="resetForm" />
+            <input type="submit" value="Proceed to Step Three &#61&#62" id="processGoogleAPI" />
+        </form>
+
+        <h3>OR</h3>
+
+        <h4>Sign in with an existing Google Account.</h4>
+
+        <?php include_once('includes/footer.php'); ?>
+    </body>
+</html>
